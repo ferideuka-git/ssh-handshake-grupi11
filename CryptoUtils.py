7d37d3f953722324c95a00ef58da92448312b782
FinesaMaxhuni <finese.maxhuni@student.uni-pr.edu>
2026-05-17 17:08:49 +0200

    Ndryshim

--- CryptoUtils.py
+++ CryptoUtils.py
@@ -1,7 +1,7 @@
- from cryptography.hazmat.primitives.asymmetric import rsa, padding, x25519
- from cryptography.hazmat.primitives import hashes, serialization
- from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
- from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives.asymmetric import rsa, padding, x25519
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.exceptions import InvalidSignature
 import os, hmac, hashlib
 
 # ================= RSA (Server Authentication) =================
