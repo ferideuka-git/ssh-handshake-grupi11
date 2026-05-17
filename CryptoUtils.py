9931ebd96d1868880475c68fa0305752c777380a
FinesaMaxhuni <finese.maxhuni@student.uni-pr.edu>
2026-05-17 17:05:03 +0200

    Krijimi i funksionit generate_rsa_keys()

--- CryptoUtils.py
+++ CryptoUtils.py
@@ -3,3 +3,10 @@
  from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
  from cryptography.exceptions import InvalidSignature
 import os, hmac, hashlib
+
+# ================= RSA (Server Authentication) =================
+def generate_rsa_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
